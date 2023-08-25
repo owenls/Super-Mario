771fb5f72cb9650f903385a91388e5e57d66845a
owenls <owen@wiredfamily.org>
2023-08-25 17:57:25 +0800

    nothing cool

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import time
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
@@ -7,10 +8,33 @@ env = gym.make("SuperMarioBros-v0",
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 done = True
 env.reset()
-for step in range(5000):
-    action = env.action_space.sample()
-    obs, reward, terminated, truncated, info = env.step(action)
-    done = terminated or truncated
+
+frame_delay = 0.01
+right_action = 1
+jump_action = 5
+
+jump_interval = 30
+
+step_counter = 0
+jump_counter = 0
+
+
+for step in range(7000):
     if done:
         state = env.reset()
-        env.close()
+
+    obs, reward, terminated, truncated, info = env.step(right_action)
+    done = terminated or truncated
+
+    if jump_counter >= jump_interval:
+        obs, reward, terminated, truncated, info = env.step(jump_action)
+        done = terminated or truncated
+        jump_counter = 0
+
+    step_counter += 1
+    jump_counter += 1
+
+    time.sleep(frame_delay)
+
+
+env.close()
