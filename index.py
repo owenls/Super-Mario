568978d08293728e92625e5916c2a1cb29ab37f7
owenls <owen@wiredfamily.org>
2023-09-05 12:37:12 +0800

    Trying to detect goombas and pipes

--- index.py
+++ index.py
@@ -3,13 +3,17 @@ from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 import gym
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
 env = gym.make("SuperMarioBros-v0",
                apply_api_compatibility=True, render_mode="human")
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 done = True
 env.reset()
 
-frame_delay = 0.01
+frame_delay = 0.05
 right_action = 1
 jump_action = 5
 
@@ -19,6 +23,23 @@ step_counter = 0
 jump_counter = 0
 
 
+def goombaCheck(obs):
+    target_color = [228, 92, 16]
+    # Select the second half of the array at y-coordinate 206 (index 205)
+    half_obs = obs[202][128:200]
+    # Check if any pixel in the selected region matches the target color
+    is_goomba_present = np.any(np.all(half_obs == target_color, axis=-1))
+    if is_goomba_present:
+        return True
+
+    target_color = [184, 248, 24]
+    # Select the second half of the array at y-coordinate 206 (index 205)
+    half_obs = obs[186][100:150]
+    # Check if any pixel in the selected region matches the target color
+    is_pipe_present = np.any(np.all(half_obs == target_color, axis=-1))
+    return is_pipe_present
+
+
 for step in range(7000):
     if done:
         state = env.reset()
@@ -26,8 +47,11 @@ for step in range(7000):
     obs, reward, terminated, truncated, info = env.step(right_action)
     done = terminated or truncated
 
-    if jump_counter >= jump_interval:
+    if goombaCheck(obs):
         obs, reward, terminated, truncated, info = env.step(jump_action)
+        obs, reward, terminated, truncated, info = env.step(jump_action)
+        obs, reward, terminated, truncated, info = env.step(jump_action)
+
         done = terminated or truncated
         jump_counter = 0
 
@@ -35,6 +59,18 @@ for step in range(7000):
     jump_counter += 1
 
     time.sleep(frame_delay)
+
+    # plt.imshow(obs)
+    # plt.grid(True)
+    # plt.show()
+    # # Convert obs to an Image object
+    # obs_image = Image.fromarray(obs)
+
+    # # Save the image to a file (optional)
+    # obs_image.save("obs.png")
+
+    # # Display the image (optional)
+    # obs_image.show()
 
 
 env.close()
